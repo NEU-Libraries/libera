29e9c882a5ae457107f9ab827bb03847b0f4f2ba
David Cliff <dgcliff@northeastern.edu>
2018-04-20 19:55:29 +0000

    Commenting out OM for now

--- lib/libera/tei.rb
+++ lib/libera/tei.rb
@@ -1,9 +1,9 @@
 module Libera
   class Tei
-    include OM::XML::Document
-    
-    set_terminology do |t|
-      t.root(:path => "TEI", :xmlns => '', :namespace_prefix => nil)
-    end
+    # include OM::XML::Document
+    # 
+    # set_terminology do |t|
+    #   t.root(:path => "TEI", :xmlns => '', :namespace_prefix => nil)
+    # end
   end
 end
