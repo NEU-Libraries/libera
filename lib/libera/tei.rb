68dd32d18ca1168811f951b72938421d80f8944a
David Cliff <dgcliff@northeastern.edu>
2018-04-23 17:21:11 +0000

    TEI stubbed out. Needed to use nokogiri hack for reserved keyword https://stackoverflow.com/questions/13530722/naming-element-text-with-nokogiri-and-ruby

--- lib/libera/tei.rb
+++ lib/libera/tei.rb
@@ -7,5 +7,19 @@ module Libera
     set_terminology do |t|
       t.root(:path => "TEI", :xmlns => '', :namespace_prefix => nil)
     end
+    
+    def self.xml_template
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.tei("xmlns"=>"http://www.tei-c.org/ns/1.0") {
+          xml.teiHeader{
+            
+          }
+          xml.text_{
+            xml.pb('facs' => '')
+          }
+        }
+      end  
+      return builder.doc
+    end
   end
 end
