6a2a7c17030730f619b91e3bfee83547b6920386
David Cliff <dgcliff@northeastern.edu>
2018-04-26 18:51:26 +0000

    WOrking on tei generation. Need to work on xpath for last pb, and add sibling for p

--- lib/libera.rb
+++ lib/libera.rb
@@ -46,12 +46,11 @@ module Libera
     # attr_accessor :example_value
     
     attr_accessor :pdf_location
-    attr_accessor :output_location
-    attr_accessor :tmp_dir
+    attr_accessor :working_dir
 
     # Default configuration values
     def initialize
-      @tmp_dir = "#{Dir.tmpdir}/libera" # Default value
+      @working_dir = "." # Default value
     end
   end
   
@@ -63,6 +62,8 @@ module Libera
       reader = PDF::Reader.new(Libera.configuration.pdf_location)
       page_count = reader.page_count - 1
       
+      page_list = Hash.new
+      
       for i in 0..page_count
         begin
           pdf = Magick::ImageList.new(Libera.configuration.pdf_location + "[#{i}]") {self.density = 300; self.quality = 100}
@@ -71,30 +72,49 @@ module Libera
           page_img.border!(0, 0, 'white')
           page_img.alpha(Magick::DeactivateAlphaChannel)
           
-          file_path = "#{Libera.configuration.tmp_dir}/images/#{Time.now.to_f.to_s.gsub!('.','')}-pdf-page-#{i}.png"
+          file_path = "#{Libera.configuration.working_dir}/images/#{Time.now.to_f.to_s.gsub!('.','')}-pdf-page-#{i}.png"
           file_list << file_path
           page_img.write(file_path) {self.depth = 8}
-          parse_image(file_path, i)
+          
+          txt = parse_image(file_path, i)
+          page_list[file_path] = txt
         ensure
           pdf.destroy! && page_img.destroy!
         end
       end
+      
+      generate_tei(page_list)
     end
     
     def parse_image(image_path, i)
-      file_path = "#{Libera.configuration.tmp_dir}/text/#{Time.now.to_f.to_s.gsub!('.','')}-pdf-page-#{i}"
+      file_path = "#{Libera.configuration.working_dir}/text/#{Time.now.to_f.to_s.gsub!('.','')}-pdf-page-#{i}"
       `tesseract #{image_path} #{file_path} >> /dev/null 2>&1`
+      
+      x = File.read(file_path)
+      txt = x.split("\n").reject { |c| c.empty? }
+      return txt
     end
     
-    def generate_tei(page_list = Hash.new)
+    def generate_tei(page_list)
+      tei_path = "#{Libera.configuration.working_dir}/tei"
       tei_xml = Libera::Tei.new
       
+      page_list.each do |k, v|
+        tei_xml.template_registry.add_child(tei_xml.ng_xml.root.elements[1], :page_break, k)
+        v.each_with_index do |str, index|
+          if index == 0
+            # tei_xml.template_registry.add_child(tei_xml.ng_xml.root.elements[1], :paragraph, str)
+          end
+          # tei_xml.template_registry.add_child(tei_xml.ng_xml.root.elements[1], :paragraph, str)
+        end
+      end
     end
     
     def mk_tmp_dirs
       # Check if dirs exist - If not, make them
-      FileUtils.mkdir("#{Libera.configuration.tmp_dir}/images") unless File.exists? "#{Libera.configuration.tmp_dir}/images"
-      FileUtils.mkdir("#{Libera.configuration.tmp_dir}/text") unless File.exists? "#{Libera.configuration.tmp_dir}/text"
+      FileUtils.mkdir("#{Libera.configuration.working_dir}/images") unless File.exists? "#{Libera.configuration.working_dir}/images"
+      FileUtils.mkdir("#{Libera.configuration.working_dir}/text") unless File.exists? "#{Libera.configuration.working_dir}/text"
+      FileUtils.mkdir("#{Libera.configuration.working_dir}/tei") unless File.exists? "#{Libera.configuration.working_dir}/tei"
     end
   end
   
