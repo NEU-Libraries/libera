25584d2e7720454551ec4143691349af29959c78
David Cliff <dgcliff@northeastern.edu>
2018-04-26 17:30:49 +0000

    Cleanup and templates for page break and paragraph

--- lib/libera/tei.rb
+++ lib/libera/tei.rb
@@ -6,26 +6,27 @@ module Libera
     
     set_terminology do |t|
       t.root(:path => "TEI", :xmlns => 'http://www.tei-c.org/ns/1.0', :namespace_prefix => nil)
-      t.page_break(path: 'pb')
-      t.paragraph(path: 'p')
+      t.txt(path: 'text'){
+        t.page_break(path: 'pb')
+        t.paragraph(path: 'p')
+      }
     end
     
-    define_template :page do |xml, img_src|
-      xml.page_break(:facs => img_src) do
-        xml.paragraph
+    define_template :page_break do |xml, img_src|
+      xml.pb(:facs => img_src)
+    end
+    
+    define_template :paragraph do |xml, p_text|
+      xml.p_ do
+        xml.text(p_text)
       end
     end
     
     def self.xml_template
       builder = Nokogiri::XML::Builder.new do |xml|
         xml.tei("xmlns"=>"http://www.tei-c.org/ns/1.0") {
-          xml.teiHeader{
-            
-          }
-          xml.text_{
-            xml.pb('facs' => '')
-            xml.p_
-          }
+          xml.teiHeader
+          xml.text_
         }
       end  
       return builder.doc
