740987bf161212b258ca626f4748be1bf1d88dfa
David Cliff <dgcliff@northeastern.edu>
2018-06-04 18:24:07 +0000

    Added template test to spec, stubbing out other spec checks

--- spec/libera_spec.rb
+++ spec/libera_spec.rb
@@ -3,7 +3,24 @@ RSpec.describe Libera do
     expect(Libera::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "produces a valid default XML template" do
+    tei_xml = Libera::Tei.new
+    expect(tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<TEI xmlns=\"http://www.tei-c.org/ns/1.0\">\n  <teiHeader>\n    <fileDesc>\n      <titleStmt>\n        <title/>\n      </titleStmt>\n      <publicationStmt>\n        <p/>\n      </publicationStmt>\n      <sourceDesc>\n        <p/>\n      </sourceDesc>\n    </fileDesc>\n  </teiHeader>\n  <text>\n    <body>\n      <ab/>\n    </body>\n  </text>\n</TEI>\n")
+  end
+  
+  it "adds page breaks" do
+    
+  end
+  
+  it "adds anon blocks" do
+    
+  end
+  
+  it "generates TEI" do
+    
+  end
+  
+  it "parses images and makes OCR text" do
+    
   end
 end
