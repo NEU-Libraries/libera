52863327cab552bc29c59cea6d8c54ea9d8a771a
David Cliff <dgcliff@northeastern.edu>
2018-06-05 19:05:15 +0000

    Added formatted XML for page break test

--- spec/libera_spec.rb
+++ spec/libera_spec.rb
@@ -18,7 +18,13 @@ RSpec.describe Libera do
     tei_xml = Libera::Tei.new
     tei_xml.add_page_break("foo")
     
-    expect(tei_xml.to_xml).to eq()
+    expect(tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"\
+      "\n<TEI xmlns=\"http://www.tei-c.org/ns/1.0\">\n  <teiHeader>\n    "\
+      "<fileDesc>\n      <titleStmt>\n        <title/>\n      "\
+      "</titleStmt>\n      <publicationStmt>\n        <p/>\n      "\
+      "</publicationStmt>\n      <sourceDesc>\n        <p/>\n     "\
+      " </sourceDesc>\n    </fileDesc>\n  </teiHeader>\n  <text>\n"\
+      "    <body>\n      <pb facs=\"foo\"/>\n    </body>\n  </text>\n</TEI>\n")
   end
   
   it "adds anon blocks" do
