a05a15db1d3f8a4b329fbe50c44d4366226c4619
David Cliff <dgcliff@northeastern.edu>
2018-06-05 14:09:22 +0000

    Breaking up spec XML for readability

--- spec/libera_spec.rb
+++ spec/libera_spec.rb
@@ -5,7 +5,13 @@ RSpec.describe Libera do
 
   it "produces a valid default XML template" do
     tei_xml = Libera::Tei.new
-    expect(tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<TEI xmlns=\"http://www.tei-c.org/ns/1.0\">\n  <teiHeader>\n    <fileDesc>\n      <titleStmt>\n        <title/>\n      </titleStmt>\n      <publicationStmt>\n        <p/>\n      </publicationStmt>\n      <sourceDesc>\n        <p/>\n      </sourceDesc>\n    </fileDesc>\n  </teiHeader>\n  <text>\n    <body>\n      <ab/>\n    </body>\n  </text>\n</TEI>\n")
+    expect(tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
+      "<TEI xmlns=\"http://www.tei-c.org/ns/1.0\">\n  <teiHeader>"\
+      "\n    <fileDesc>\n      <titleStmt>\n        <title/>\n   "\
+      "   </titleStmt>\n      <publicationStmt>\n        <p/>\n  "\
+      "    </publicationStmt>\n      <sourceDesc>\n        <p/>\n"\
+      "      </sourceDesc>\n    </fileDesc>\n  </teiHeader>\n"\
+      "  <text>\n    <body>\n      <ab/>\n    </body>\n  </text>\n</TEI>\n")
   end
   
   it "adds page breaks" do
