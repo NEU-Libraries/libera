e0b9f1777eafd1bf5b074b6d52fcd272127e9020
David Cliff <dgcliff@northeastern.edu>
2018-06-06 18:19:07 +0000

    Before and after blocks for instantiation and cleanup

--- spec/libera_spec.rb
+++ spec/libera_spec.rb
@@ -1,11 +1,21 @@
 RSpec.describe Libera do
+  
+  before(:each) do
+    @tei_xml = Libera::Tei.new
+    @parser = Libera::Parser.new
+    @parser.mk_working_dir
+  end
+  
+  after(:each) do
+    FileUtils.rm_rf(Libera.configuration.working_dir)
+  end
+  
   it "has a version number" do
     expect(Libera::VERSION).not_to be nil
   end
 
   it "produces a valid default XML template" do
-    tei_xml = Libera::Tei.new
-    expect(tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
+    expect(@tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
       "<TEI xmlns=\"http://www.tei-c.org/ns/1.0\">\n  <teiHeader>"\
       "\n    <fileDesc>\n      <titleStmt>\n        <title/>\n   "\
       "   </titleStmt>\n      <publicationStmt>\n        <p/>\n  "\
@@ -15,10 +25,9 @@ RSpec.describe Libera do
   end
   
   it "adds page breaks" do
-    tei_xml = Libera::Tei.new
-    tei_xml.add_page_break("foo")
+    @tei_xml.add_page_break("foo")
     
-    expect(tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"\
+    expect(@tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"\
       "\n<TEI xmlns=\"http://www.tei-c.org/ns/1.0\">\n  <teiHeader>\n    "\
       "<fileDesc>\n      <titleStmt>\n        <title/>\n      "\
       "</titleStmt>\n      <publicationStmt>\n        <p/>\n      "\
@@ -28,11 +37,10 @@ RSpec.describe Libera do
   end
   
   it "adds anon blocks" do
-    tei_xml = Libera::Tei.new
-    tei_xml.add_page_break("foo")
-    tei_xml.add_anon_block("bar")
+    @tei_xml.add_page_break("foo")
+    @tei_xml.add_anon_block("bar")
     
-    expect(tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
+    expect(@tei_xml.to_xml).to eq("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
       "<TEI xmlns=\"http://www.tei-c.org/ns/1.0\">\n  <teiHeader>\n    "\
       "<fileDesc>\n      <titleStmt>\n        <title/>\n      </titleStmt>\n"\
       "      <publicationStmt>\n        <p/>\n      </publicationStmt>\n    "\
@@ -42,19 +50,18 @@ RSpec.describe Libera do
   end
   
   it "errors on adding anon blocks without a page break" do
-    tei_xml = Libera::Tei.new
-    expect { tei_xml.add_anon_block("bar") }.to raise_error(RuntimeError)
+    # tei_xml = Libera::Tei.new
+    expect { @tei_xml.add_anon_block("bar") }.to raise_error(RuntimeError)
   end
   
   it "generates TEI" do
-    parser = Libera::Parser.new
+    # TODO
   end
   
   it "parses images and makes OCR text" do
-    parser = Libera::Parser.new
-    parser.mk_working_dir
+    @parser.mk_working_dir
     
-    txt = parser.parse_image("./test/fixtures/test.png", 0)
+    txt = @parser.parse_image("./test/fixtures/test.png", 0)
     
     expect(txt).to eq("I am all manner of test\ndocument.\n\nLorem ipsum lorem\n"\
       "ipsum lorem ipsom.\n\nAsdfasdfasdf\nAsdfasdfasdf\n\n")
